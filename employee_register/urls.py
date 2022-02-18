44272702810920d2e24a853a28036aebb643fc00
Nakaw <nakawyoung@gmail.com>
2022-02-18 15:36:23 -0500

    added .env file

---
+++ employee_register/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path, include
+from . import views
+urlpatterns = [
+    path('', views.employee_form),
+    path('list',views.employee_list)
+]
---
+++ employee_register/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .forms import EmployeeForm
+
+# Create your views here.
+
+def employee_list(request):
+    return render (request,"employee_register/employee_list.html")
+
+def employee_form(request):
+    form = EmployeeForm()
+    return render(request,"employee_register/employee_form.html",{"form":form})
+
+def employee_delete(request):
+    return

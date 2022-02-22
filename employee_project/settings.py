3b0fdece59ac9ccf4fc395a18437e1770daa61c7
Nakaw <nakawyoung@gmail.com>
2022-02-22 12:27:03 -0500

    Added styling for form and added migrations

--- employee_project/settings.py
+++ employee_project/settings.py
@@ -39,8 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'employee_register'
+    'employee_register',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',

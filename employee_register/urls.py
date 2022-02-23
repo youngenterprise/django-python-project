1edbdc746780ea728d5c5d9b51e795cbfa1cff51
Nakaw <nakawyoung@gmail.com>
2022-02-23 12:07:02 -0500

    Added delete operations and enabled easier routing through the app

--- employee_register/urls.py
+++ employee_register/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = [
     path('', views.employee_form, name='employee_insert'), # get and post for insert operation
     path('<int:id>/', views.employee_form, name='employee_update'), #get and post for update
-    path('list',views.employee_list, name='employee_list') #get req to display all records
+    path('list',views.employee_list, name='employee_list'), #get req to display all records
+    path('delete/<int:id>/', views.employee_delete, name='employee_delete')
 ]

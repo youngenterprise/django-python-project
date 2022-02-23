ba2af12fefc819de1ec26b2e038bbb9e74a36bc1
Nakaw <nakawyoung@gmail.com>
2022-02-22 22:48:14 -0500

    added routes for update and get request for all data

--- employee_register/urls.py
+++ employee_register/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('', views.employee_form),
-    path('list',views.employee_list)
+    path('', views.employee_form, name='employee_insert'), # get and post for insert operation
+    path('<int:id>/', views.employee_form, name='employee_update'), #get and post for update
+    path('list',views.employee_list, name='employee_list') #get req to display all records
 ]
--- employee_register/views.py
+++ employee_register/views.py
@@ -7,9 +7,12 @@ def employee_list(request):
     context = {'employee_list': Employee.objects.all()}
     return render (request, "employee_register/employee_list.html", context)
 
-def employee_form(request):
+def employee_form(request, id=0):
     if request.method == "GET":
-        form = EmployeeForm()
+        if id==0:
+            form = EmployeeForm()
+        else:
+            employee = Employee.objects.get(pk=id)
         return render(request,"employee_register/employee_form.html",{"form":form})
     else:
         form = EmployeeForm(request.POST)
